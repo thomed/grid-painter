9852e9398ffb42e7a13da9bf379250a8f6e67d73
thomed <thomasedwalker@gmail.com>
2017-02-05 16:34:33 -0700

    style and code readability changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-var weeDiv = "<div class='wee-div'></div>";
-var rowDiv = "<div class='row grid-row'></div>";
+// ingredients
 var col = 1, row = 1;
 var numColumns = 50, numRows = 50;
 var mouseDown = false;
 var color = "#00f";
 
+// html div strings
+var weeDiv = "<div class='wee-div'></div>";
+var rowDiv = "<div class='row grid-row'></div>";
+
+// html div classes
+var divColClass = "column-" + col, divRowClass = "row-" + row;
 
 $(document).ready(function () {
     initGrid();
@@ -18,18 +23,15 @@ $(document).ready(function () {
     
     $("#color-text").change(function() {
         color = "#" + $("#color-text").val();
-        console.log("color update to " + color);
-    })
+    });
 });
 
 function initGrid() {
     for (gridRow = 0; gridRow < numRows; gridRow++) {
         $("#grid-area").append(rowDiv);
         for (gridColumn = 0; gridColumn < numColumns; gridColumn++) {
-            var divColClass = "column-" + col;
-            var divRowClass = "row-" + row;
-            $("#grid-area").children().last().append(weeDiv);
-            $("#grid-area").children().last().children().last().addClass(divRowClass + " " + divColClass);
+            getLastChild().append(weeDiv);
+            getLastGrandchild().addClass(divRowClass + " " + divColClass);
             col++;
         }
         col = 0;
@@ -50,5 +52,12 @@ function initGrid() {
 
 function clearCanvas() {
     $(".wee-div").css({'background-color': 'white'});
-    console.log("cleared");
+}
+
+function getLastChild() {
+    return $("#grid-area").children().last();
+}
+
+function getLastGrandchild() {
+    return $("#grid-area").children().last().children().last();
 }
