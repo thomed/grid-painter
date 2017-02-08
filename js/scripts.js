cb517d7cf596af5d4eddda2e66eec01bac886c78
Thomas Walker <u0717987@ad.utah.edu>
2017-02-08 16:03:34 -0700

    first steps toward writing rgb values to canvas

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,10 @@
 
 // ingredients
 var col = 1, row = 1;
-var numColumns = 50, numRows = 50;
+var numColumns = 20, numRows = 20;
 var mouseDown = false;
 var color = "#00f";
+var gridArr;
 
 // html div strings
 var weeDiv = "<div class='wee-div'></div>";
@@ -33,6 +34,7 @@ $(document).ready(function () {
         color = "#" + $("#color-text").val();
     });
 
+    // increase cell size with slider
     $("#scale-slider").slider({
         value: 10,
         min: 8,
@@ -43,6 +45,7 @@ $(document).ready(function () {
         }
     });
 
+    // enable/disable grid with checkbox
     $("#grid-checkbox").change(function () {
         if (!$("#grid-checkbox").is(":checked")) {
             $(".wee-div").css({"border": "none"});
@@ -54,16 +57,33 @@ $(document).ready(function () {
 });
 
 function initGrid() {
+    gridArr = new Array(numColumns);
+
     for (gridRow = 0; gridRow < numRows; gridRow++) {
+        gridArr[gridRow] = new Array(numRows);
+
         $("#grid-area").append(rowDiv);
         for (gridColumn = 0; gridColumn < numColumns; gridColumn++) {
-            getLastChild().append(weeDiv);
+            gridArr[gridRow][gridColumn] = getLastChild().append(weeDiv).children().last();
             getLastGrandchild().addClass(divRowClass + " " + divColClass);
             col++;
         }
         col = 0;
         row++;
     }
+    
+// rgb color info parsing 
+// TODO probably do something along these lines to store rgb info with each grid element
+//      then write to location on canvas
+    console.log(gridArr[0][19].css("background-color"));
+    gridArr[0][19].backgroundColor = gridArr[0][19].css("background-color").slice(4, 7) + gridArr[0][19].css("background-color").slice(9, 12) + gridArr[0][19].css("background-color").slice(14, 17);
+    var rawRGB = gridArr[0][19].css("background-color").split(",");
+    gridArr[0][19].r = rawRGB[0].trim().slice(rawRGB[0].indexOf("(") + 1, rawRGB[0].length);
+    gridArr[0][19].g = rawRGB[1].trim();
+    gridArr[0][19].b = rawRGB[2].trim().slice(0, rawRGB[2].indexOf(")") - 1);
+    console.log(gridArr[0][19].r + " " + gridArr[0][19].g + " " + gridArr[0][19].b);
+// end rgb format prototyping
+// probably best to stick with this kind of a method, at least for initial implementation.
 
     $(".grid-row").children().each(function () {
         $(this).mouseover(function () {
@@ -102,7 +122,10 @@ function getLastGrandchild() {
 // >>>> https://www.w3.org/TR/PNG-Chunks.html <<<< chunks
 //
 // look into using btoa() or atob()?
-
+// 
+// alternate solution: draw to a hidden canvas then return canvas.toDataURL()
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// probably the simplest out of the other solutions, but not as neat.
 function generateURI() {
     var headerInfo = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA";
     return headerInfo + "EAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY7D3OAMAAh0BVEDFEogAAAAASUVORK5CYII=";
@@ -117,6 +140,18 @@ function downloadPNG(uri) {
     document.body.removeChild(link);
     delete link;
     console.log(uri);
+}
+
+// generate (but don't display!) canvas.
+// then get a data URI/URL for the canvas which can then be applied to the 
+// download link
+function generateCanvas() {
+    var canvas = document.createElement("canvas");
+    canvas.width = numColumns;
+    canvas.height = numRows;
+    document.body.appendChild(canvas);
+
+    return canvas.toDataURL("image/png");
 }
 
 $("#download-btn").click(function () {
